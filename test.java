d1578626c99ca51dba6ad328c59b3a6cac84d27f
XYplayer0 <102475597+XYplayer0@users.noreply.github.com>
2024-09-26 21:12:23 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 package com.itheima.study1;
 
 public class test {
-  //Hi, this is a test to find out how this website work
+  /*Hi, this is a test to find out how this website work
+  And because this idea just came to me recently, so here has nothing yet, but I will post my self learning result here in the future.*/
 }
